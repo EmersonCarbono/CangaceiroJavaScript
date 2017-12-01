367addb651609da7e4665ee24957b6b21f2d0140
emersoncarbono <emersonscarbonaro@gmail.com>
2017-12-01 10:24:23 -0200

    Melhorando o codigo passando o querySelector para variavel $

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,18 +1,24 @@
 class NegociacaoController{
     adicionar(){
         
-        //Cancelar a submissão do evento, não atualiza a tela quando da submit
+        // Cancelar a submissão do evento, não atualiza a tela quando da submit
         event.preventDefault();
         
-        
-        //Pegar os elementos
-        let inputData = document.querySelector('#data');
-        let inputQuantidade = document.querySelector('#quantidade');
-        let inputValor = document.querySelector('#valor');
+        // Pegar os elementos
+        // Por padrão o querySelector retorna os valores em String, lembre-se de converter
+        // No JS podemos adicionar funções as variaveis então podemos reduzir nosso codigo
+        // Realizando o bind, $ mantém	document com o seu contexto this
+        let $ = document.querySelector.bind(document);
+
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
 
         console.log(inputData.value);
         console.log(parseInt(inputQuantidade.value));
         console.log(parseFloat(inputValor.value));
+
+
 
         alert('Chamei a ação do controle');
     }
